449093e7543fb6d5f683a9556198226c61b44b24
Nicolás Jurado <nicomacojurado@gmail.com>
2022-08-28 22:07:13 -0500

    Versión final 1: comentado y organizado
    Esta es la primera versión completa del código de CategoríaProductos en el que se tienen los requerimientos solucionados!
    
    BTW, VER LÍNEAS 152 A 161 EN Empresa.java (Quedó lindo ^^)
    
    __________________
    
    Falta:
    1. Arreglar para que entre a todas las categorías con un ciclo.

--- src/catProds/Empresa.java
+++ src/catProds/Empresa.java
@@ -29,44 +29,53 @@ public class Empresa {
 		this.listaCategorias = listaCategorias;
 	}
 
+	// ---------------------------------------------------------------------
+	// AQUÍ EMPIEZA LA LÓGICA
+
 	public boolean encontrarProductoColor(String color, Categoria cat, int i, int j) {
 
-		// System.out.println("Valor de i: " + i);
-		// System.out.println("Valor de j: " + j);
-
+		// ESTA ES LA LÓGICA PARA UNA CATEGORÍA EN ESPECÍFICO SIN TOMAR EN
+		// CUENTA LAS OTRAS CATEGORÍAS, SOLO LOS PRODUCTOS
 		if (cat.getListaProductos() != null) {
+			// Revisa si tiene productos dentro
 			if (j <= cat.getListaProductos().size() - 1) {
-
+				// Compara a j para que no se desborde con el arraylist de productos
 				if (cat.getListaProductos().get(j).getColor().equals(color)) {
-					// System.out.println("Entra al producto " + j);
+					// Entra al producto j;
 					return true;
 				}
 
 				else {
-					// System.out.println("Entra al producto " + (j + 1));
+					// Entra al producto siguiente en caso de no encontrarlo
 					boolean a = encontrarProductoColor(color, cat, i, j + 1);
 					if (a) {
+						// Si devuelve verdadero, que retorne verdadero
 						return true;
 					}
 				}
 			}
 		}
 
+		// AQUÍ EMPIEZA LA LÓGICA PARA RECORRER LAS CATEGORÍAS INTERNAS
+		// EN CASO TAL DE QUE TENGA
 		if (cat.getListaCategorias() != null) {
-			j = 0;
+			// Mira si la lista de categorías no sea nula
+			j = 0; // Se debe reiniciar j porque es una nueva categoría
 			int tam = cat.getListaCategorias().size();
-			// System.out.println("Tiene subcategorías: " + tam);
+			// Tiene subcategorías de tamaño tam;
 
 			if (i <= (tam - 1)) {
-				// System.out.println("Entra a la categoría " + i);
+				// Compara que la i no se desborde del arraylist
+				// Entra a la categoría i
 				if (encontrarProductoColor(color, cat.getListaCategorias().get(i), i, j)) {
-					// System.out.println("La categoría no tiene hermanas");
+					// La categoría no tiene hermanas
 					return true;
 				} else {
 					if (i + 1 <= (tam - 1)) {
-						// System.out.println("Entra a la categoría " + (i + 1));
+						// Compara que exista una siguiente categoría
+						// Entra a la categoría siguiente en caso de tener;
 						if (encontrarProductoColor(color, cat.getListaCategorias().get(i + 1), i, j)) {
-							// System.out.println("La categoría tiene hermanas");
+							// La categoría tiene hermanas
 							return true;
 						}
 
@@ -77,58 +86,76 @@ public class Empresa {
 
 		}
 
+		// En caso de no encontrar ningún producto con tal color, devuelve falso
 		return false;
 
 	}
 
 	public int encontrarProductoMayorA(int precio, Categoria cat, int i, int j, int cantidad) {
 
-		// System.out.println("Valor de i: " + i);
-		// System.out.println("Valor de j: " + j);
-
+		// Esto es la cantidad acumulada de productos con precios mayores o iguales al
+		// precio solicitado
 		int cant = cantidad;
 
+		// ESTA ES LA LÓGICA PARA UNA CATEGORÍA EN ESPECÍFICO SIN TOMAR EN
+		// CUENTA LAS OTRAS CATEGORÍAS, SOLO LOS PRODUCTOS
 		if (cat.getListaProductos() != null) {
+			// Mira si la lista de los productos no es nula
 			if (j <= cat.getListaProductos().size() - 1) {
-
+				// Compara a j para que no se desborde con el arraylist de productos
 				if (cat.getListaProductos().get(j).getPrecio() >= (precio)) {
-					// System.out.println("Entra al producto " + j);
+					// Entra al producto j
 					return ((cant + 1) + encontrarProductoMayorA(precio, cat, i, j + 1, cant));
+					// Retorna la cantidad más uno en caso de que la condición se cumpla, y
+					// adicionalmente se pasa
+					// al siguiente producto y lo suma a la cantidad anterior
 				}
 			}
 
 		}
 
+		// AQUÍ EMPIEZA LA LÓGICA PARA RECORRER LAS CATEGORÍAS INTERNAS
+		// EN CASO TAL DE QUE TENGA
 		if (cat.getListaCategorias() != null) {
-			j = 0;
+			j = 0; // Se debe reiniciar j porque es una nueva categoría
 			int tam = cat.getListaCategorias().size();
-			// System.out.println("Tiene subcategorías: " + tam);
+			// Tiene subcategorías de tamaño tam;
 
 			if (i <= (tam - 1)) {
-				// System.out.println("Entra a la categoría " + i);
-				// System.out.println("La categoría no tiene hermanas");
+				// Compara que la i no se desborde del arraylist
+				// Entra a la categoría i
 
-				int val = cant;
+				int val = cant; // Es una variable local para sumar en caso de que tenga o no más categorías
+								// En caso de que sí tenga, se le suma a val la cantidad retornada por la
+								// función
 				val = encontrarProductoMayorA(precio, cat.getListaCategorias().get(i), i, j, cant);
 				if (i + 1 <= (tam - 1)) {
+					// Compara que exista una siguiente categoría
+					// Entra a la categoría siguiente en caso de tener;
 					val += encontrarProductoMayorA(precio, cat.getListaCategorias().get(i + 1), i, j, cant);
-					return val;
+					return val; // Retorna val en caso de que sí hayan categorías hermanas
 				} else {
-					return val;
+					return val; // Retorna val en caso de que no hayan categorías hermanas, porque si no,
+								// queda sin sumarse ese valor
 				}
 
 			}
 
 		}
 
+		// Retorna la cantidad final en caso de no entrar a ninguna condición
 		return cant;
 
 	}
 }
 
-// Sacar los valores que tiene esa categoría sin ver si tiene más categorías.
-// Ya, se mira luego si tiene más categorías y en caso de que tenga, se ingresa
-// a la primera categoría y se hace lo mismo del principio.
-// Cuando no hayan más categorías bajo de esa, se sigue recorriendo la siguiente
-// categoría del nivel anterior, es decir, si estaba en la categoría cero, pasa
-// a la categoría 1
+// ---------------------------------------------------------------------------------
+// | Esta es la lógica en la cual me basé para solucionar el problema:			   |
+// |-------------------------------------------------------------------------------|
+// | Sacar los valores que tiene esa categoría sin ver si tiene más categorías.    |
+// | Ya, se mira luego si tiene más categorías y en caso de que tenga, se ingresa  |
+// | a la primera categoría y se hace lo mismo del principio.                      |
+// | Cuando no hayan más categorías bajo de esa, se sigue recorriendo la siguiente |
+// | categoría del nivel anterior, es decir, si estaba en la categoría cero, pasa  |
+// | a la categoría 1                         									   |
+// ---------------------------------------------------------------------------------
--- src/catProds/Main.java
+++ src/catProds/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// Productos Envasados:
 		Producto prod1 = new Producto("Verde", 2000);
 		Producto prod2 = new Producto("Azul", 5000);
 		Producto prod3 = new Producto("Gris", 5000);
@@ -17,6 +18,8 @@ public class Main {
 		Producto prod7 = new Producto("Verde", 100);
 		Producto prod8 = new Producto("Amarillo", 1000);
 		Producto prod9 = new Producto("Rojo", 500);
+
+		// Productos Congelados:
 		Producto prod10 = new Producto("Lila", 10000);
 
 		ArrayList<Producto> productos1 = new ArrayList<Producto>();
@@ -25,33 +28,56 @@ public class Main {
 		ArrayList<Producto> productos4 = new ArrayList<Producto>();
 		ArrayList<Producto> productos5 = new ArrayList<Producto>();
 
+		// LOS GENERALES SIGNIFICA QUE NO TIENEN UNA SUBCATEGORÍA
+		// Productos Envasados Generales:
 		productos1.add(prod1);
 		productos1.add(prod2);
+
+		// Productos Envasados Nacionales Generales:
 		productos2.add(prod3);
 		productos2.add(prod4);
+
+		// Productos Envasados Importados Generales:
 		productos3.add(prod5);
 		productos3.add(prod6);
+
+		// Productos Envasados Importados Europeos:
 		productos4.add(prod7);
 		productos4.add(prod8);
 		productos4.add(prod9);
+
+		// Productos Congelados Generales:
 		productos5.add(prod10);
+
+		// PARA LEER: LAS PARTES QUE TIENEN PARÉNTESIS EN ESTE MENSAJE SIGNIFICAN EL
+		// NIVEL DE LA CATEGORÍA
+		// ENTRE MÁS ALTO, MENOS NIVEL TIENE
+		// Los arraylists de productos de 1 a 4 pertenecen a la categoría de envasados
+		// Más a fondo, productos1 pertenece a la general de envasados; productos2
+		// pertenece a la subcategoría(1) de nacionales;
+		// productos3 pertenece a la general de la subcategoría (1) de importados,
+		// mientras que productos 4 pertenece a la
+		// subcategoría (2), es decir dentro de catImportados, donde están los europeos;
+		// ya, finalizando por productos5, está en la
+		// general de la categoría de los congelados, el cual está en el mismo nivel de
+		// la categoría envasados.
 
 		ArrayList<Categoria> catEnvasados = new ArrayList<Categoria>();
 		ArrayList<Categoria> catCongelados = new ArrayList<Categoria>();
 		ArrayList<Categoria> catImportados = new ArrayList<Categoria>();
 
-		Categoria subCat3 = new Categoria("Europeos", null, productos3);
+		Categoria subCat3 = new Categoria("Europeos", null, productos4);
 
 		catImportados.add(subCat3);
 
 		Categoria subCat1 = new Categoria("Nacionales", null, productos2);
-		Categoria subCat2 = new Categoria("Importados", catImportados, productos5);
+		Categoria subCat2 = new Categoria("Importados", catImportados, productos3);
 
 		catEnvasados.add(subCat1);
 		catEnvasados.add(subCat2);
 
 		Categoria envasados = new Categoria("Envasados", catEnvasados, productos1);
-		Categoria congelados = new Categoria("Congelados", null, productos4);
+		Categoria congelados = new Categoria("Congelados", null, productos5);
 
 		ArrayList<Categoria> categorias = new ArrayList<Categoria>();
 
@@ -60,7 +86,7 @@ public class Main {
 
 		Empresa empresa = new Empresa("ACME", categorias);
 
-		// NOTA: TANTO LA FUNCIÓN DE ENCONTRAR EL PRODUCTO CON TAL COLOR Y SABER LA
+		// TANTO LA FUNCIÓN DE ENCONTRAR EL PRODUCTO CON TAL COLOR Y SABER LA
 		// CANTIDAD DE PRODUCTOS CON UN COLOR
 		// SOLO FUNCIONAN CON UNA CATEGORÍA EN ESPECÍFICO, YA TOCARÍA HACER UN FOR PARA
 		// RECORRER LAS DOS CATEGORÍAS
