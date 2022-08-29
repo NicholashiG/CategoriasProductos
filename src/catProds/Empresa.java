a6854c7250cbf2d91a874878854786935aea01d2
Nicolás Jurado <nicomacojurado@gmail.com>
2022-08-28 21:31:20 -0500

    Las dos funciones están implementadas
    Como dije en el Main (líneas 63 a 67), solo sirven con una categoría en específico.
    
    En la siguiente versión estará comentado el código para más entendimiento

--- src/catProds/Empresa.java
+++ src/catProds/Empresa.java
@@ -31,27 +31,19 @@ public class Empresa {
 
 	public boolean encontrarProductoColor(String color, Categoria cat, int i, int j) {
 
-		// Sacar los valores que tiene esa categoría sin ver si tiene más categorías.
-		// Ya, se mira luego si tiene más categorías y en caso de que tenga, se ingresa
-		// a la primera categoría y se hace lo mismo del principio.
-		// Cuando no hayan más categorías bajo de esa, se sigue recorriendo la siguiente
-		// categoría del nivel anterior, es decir, si estaba en la categoría cero, pasa
-		// a la categoría 1
-		// -------------------------------------
-
-		//System.out.println("Valor de i: " + i);
-		//System.out.println("Valor de j: " + j);
+		// System.out.println("Valor de i: " + i);
+		// System.out.println("Valor de j: " + j);
 
 		if (cat.getListaProductos() != null) {
 			if (j <= cat.getListaProductos().size() - 1) {
 
 				if (cat.getListaProductos().get(j).getColor().equals(color)) {
-					//System.out.println("Entra al producto " + j);
+					// System.out.println("Entra al producto " + j);
 					return true;
 				}
 
 				else {
-					//System.out.println("Entra al producto " + (j + 1));
+					// System.out.println("Entra al producto " + (j + 1));
 					boolean a = encontrarProductoColor(color, cat, i, j + 1);
 					if (a) {
 						return true;
@@ -63,18 +55,18 @@ public class Empresa {
 		if (cat.getListaCategorias() != null) {
 			j = 0;
 			int tam = cat.getListaCategorias().size();
-			//System.out.println("Tiene subcategorías: " + tam);
+			// System.out.println("Tiene subcategorías: " + tam);
 
 			if (i <= (tam - 1)) {
-				//System.out.println("Entra a la categoría " + i);
+				// System.out.println("Entra a la categoría " + i);
 				if (encontrarProductoColor(color, cat.getListaCategorias().get(i), i, j)) {
-					//System.out.println("La categoría no tiene hermanas");
+					// System.out.println("La categoría no tiene hermanas");
 					return true;
 				} else {
 					if (i + 1 <= (tam - 1)) {
-						//System.out.println("Entra a la categoría " + (i + 1));
+						// System.out.println("Entra a la categoría " + (i + 1));
 						if (encontrarProductoColor(color, cat.getListaCategorias().get(i + 1), i, j)) {
-							//System.out.println("La categoría tiene hermanas");
+							// System.out.println("La categoría tiene hermanas");
 							return true;
 						}
 
@@ -85,91 +77,58 @@ public class Empresa {
 
 		}
 
-		// -------------------------------------
-
-		/*
-		 * 
-		 * j = 0; Categoria subCat; subCat = cat.getListaCategorias().get(i);
-		 * System.out.println(subCat.getNombre()); boolean a =
-		 * encontrarProductoColor(color, subCat, i+1, j); if (a) { return true; } else {
-		 * a = encontrarProductoColor(color, subCat, i+2, j); if (a) { return true; } }
-		 * 
-		 * 
-		 * 
-		 * 
-		 * Categoria categ;
-		 * 
-		 * boolean x = false;
-		 * 
-		 * System.out.println("Valor de i: " + i); System.out.println("Valor de j: " +
-		 * j);
-		 * 
-		 * if (cat.getListaCategorias() != null) {
-		 * 
-		 * if (i <= cat.getListaCategorias().size() - 1) {
-		 * 
-		 * if (j <= cat.getListaProductos().size() - 1) {
-		 * 
-		 * if (cat.getListaProductos().get(j).getColor().equals(color)) {
-		 * System.out.println("Entra al producto x " + j);
-		 * 
-		 * return true; }
-		 * 
-		 * else { System.out.println("Entra al producto xx" + (j + 1)); x =
-		 * encontrarProductoColor(color, cat, i, j + 1); if (x == true) { return true; }
-		 * } }
-		 * 
-		 * j = 0;
-		 * 
-		 * categ = cat.getListaCategorias().get(i);
-		 * System.out.println("Entra en la categoria " + i);
-		 * 
-		 * if (categ.getListaProductos() != null) {
-		 * 
-		 * if (i <= categ.getListaProductos().size() - 1) {
-		 * 
-		 * if (categ.getListaProductos().get(j).getColor().equals(color)) {
-		 * System.out.println("Entra al producto " + j); return true;
-		 * 
-		 * }
-		 * 
-		 * else {
-		 * 
-		 * System.out.println("Entra al producto dentro de la categoria " + (i));
-		 * 
-		 * x = encontrarProductoColor(color, categ, i, j + 1); if (x == true) { return
-		 * true; } else { System.out.println("Entra al hijo" + (i + 1));
-		 * encontrarProductoColor(color, cat, i + 1, 0); } }
-		 * 
-		 * } }
-		 * 
-		 * }
-		 * 
-		 * }
-		 * 
-		 * else {
-		 *
-		 * System.out.
-		 * println("Entra a los productos porque no hay más categorías dentro");
-		 * 
-		 * if (j <= cat.getListaProductos().size() - 1) {
-		 * 
-		 * if (cat.getListaProductos().get(j).getColor().equals(color)) {
-		 * System.out.println("Entra al producto x " + j);
-		 * 
-		 * return true; }
-		 * 
-		 * else { System.out.println("Entra al producto xx" + (j + 1)); x =
-		 * encontrarProductoColor(color, cat, i, j + 1); if (x == true) { return true; }
-		 * } } } System.out.println("Retorna falso y no entra a ninguna condición");
-		 * return x;
-		 */
 		return false;
 
 	}
 
-	public void encontrarPrecioMayorA(int precio, Categoria cat) {
+	public int encontrarProductoMayorA(int precio, Categoria cat, int i, int j, int cantidad) {
+
+		// System.out.println("Valor de i: " + i);
+		// System.out.println("Valor de j: " + j);
+
+		int cant = cantidad;
+
+		if (cat.getListaProductos() != null) {
+			if (j <= cat.getListaProductos().size() - 1) {
+
+				if (cat.getListaProductos().get(j).getPrecio() >= (precio)) {
+					// System.out.println("Entra al producto " + j);
+					return ((cant + 1) + encontrarProductoMayorA(precio, cat, i, j + 1, cant));
+				}
+			}
+
+		}
+
+		if (cat.getListaCategorias() != null) {
+			j = 0;
+			int tam = cat.getListaCategorias().size();
+			// System.out.println("Tiene subcategorías: " + tam);
+
+			if (i <= (tam - 1)) {
+				// System.out.println("Entra a la categoría " + i);
+				// System.out.println("La categoría no tiene hermanas");
+
+				int val = cant;
+				val = encontrarProductoMayorA(precio, cat.getListaCategorias().get(i), i, j, cant);
+				if (i + 1 <= (tam - 1)) {
+					val += encontrarProductoMayorA(precio, cat.getListaCategorias().get(i + 1), i, j, cant);
+					return val;
+				} else {
+					return val;
+				}
+
+			}
+
+		}
+
+		return cant;
 
 	}
-
 }
+
+// Sacar los valores que tiene esa categoría sin ver si tiene más categorías.
+// Ya, se mira luego si tiene más categorías y en caso de que tenga, se ingresa
+// a la primera categoría y se hace lo mismo del principio.
+// Cuando no hayan más categorías bajo de esa, se sigue recorriendo la siguiente
+// categoría del nivel anterior, es decir, si estaba en la categoría cero, pasa
+// a la categoría 1
--- src/catProds/Main.java
+++ src/catProds/Main.java
@@ -2,23 +2,22 @@ package catProds;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		Producto prod1 = new Producto("Verde", 12000);
-		Producto prod2 = new Producto("Azul", 1000);
+		Producto prod1 = new Producto("Verde", 2000);
+		Producto prod2 = new Producto("Azul", 5000);
 		Producto prod3 = new Producto("Gris", 5000);
-		Producto prod4 = new Producto("Negro", 17000);
-		Producto prod5 = new Producto("Rojo", 8000);
-		Producto prod6 = new Producto("Amarillo", 9000);
-		Producto prod7 = new Producto("Verde", 3000);
+		Producto prod4 = new Producto("Negro", 30000);
+		Producto prod5 = new Producto("Rojo", 5000);
+		Producto prod6 = new Producto("Amarillo", 19000);
+		Producto prod7 = new Producto("Verde", 100);
 		Producto prod8 = new Producto("Amarillo", 1000);
 		Producto prod9 = new Producto("Rojo", 500);
-		Producto prod10 = new Producto("Lila", 100);
-
-
-		// Hay 2 productos mayores a 10000
+		Producto prod10 = new Producto("Lila", 10000);
 
 		ArrayList<Producto> productos1 = new ArrayList<Producto>();
 		ArrayList<Producto> productos2 = new ArrayList<Producto>();
@@ -35,15 +34,13 @@ public class Main {
 		productos4.add(prod7);
 		productos4.add(prod8);
 		productos4.add(prod9);
-		productos5.add(prod10);		
+		productos5.add(prod10);
 
 		ArrayList<Categoria> catEnvasados = new ArrayList<Categoria>();
 		ArrayList<Categoria> catCongelados = new ArrayList<Categoria>();
 		ArrayList<Categoria> catImportados = new ArrayList<Categoria>();
 
-
 		Categoria subCat3 = new Categoria("Europeos", null, productos3);
-
 
 		catImportados.add(subCat3);
 
@@ -62,16 +59,48 @@ public class Main {
 		categorias.add(envasados);
 
 		Empresa empresa = new Empresa("ACME", categorias);
-				
-		Boolean colorExists = (empresa.encontrarProductoColor("Lila", envasados, 0, 0));
+
+		// NOTA: TANTO LA FUNCIÓN DE ENCONTRAR EL PRODUCTO CON TAL COLOR Y SABER LA
+		// CANTIDAD DE PRODUCTOS CON UN COLOR
+		// SOLO FUNCIONAN CON UNA CATEGORÍA EN ESPECÍFICO, YA TOCARÍA HACER UN FOR PARA
+		// RECORRER LAS DOS CATEGORÍAS
+		// DENTRO DE LA EMPRESA, MIENTRAS SE QUEDA ASÍ
+
+		String color = JOptionPane.showInputDialog("Ingrese el color a saber");
+
+		Boolean colorExists = (empresa.encontrarProductoColor(color, envasados, 0, 0));
 		if (colorExists) {
-			System.out.println("El color solicitado existe dentro del inventario");
-		}
-		else {
-			System.out.println("El color solicitado NO existe dentro del inventario");
+
+			JOptionPane.showMessageDialog(null, "El color " + color.toLowerCase() + " existe dentro del inventario");
+
+		} else {
+
+			JOptionPane.showMessageDialog(null, "El color " + color.toLowerCase() + " NO existe dentro del inventario");
 
 		}
-		
+
+		int precio = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el precio a saber"));
+
+		int cantProductosMayorExists = (empresa.encontrarProductoMayorA(precio, envasados, 0, 0, 0));
+
+		if (cantProductosMayorExists > 0) {
+
+			if (cantProductosMayorExists == 1) {
+
+				JOptionPane.showMessageDialog(null,
+						"Hay " + cantProductosMayorExists + " producto con precio mayor o igual a $" + precio);
+
+			} else {
+
+				JOptionPane.showMessageDialog(null,
+						"Hay " + cantProductosMayorExists + " productos con precio mayor o igual a $" + precio);
+
+			}
+		} else {
+
+			JOptionPane.showMessageDialog(null, "No hay productos mayores o iguales a $" + precio);
+
+		}
 
 	}
 
